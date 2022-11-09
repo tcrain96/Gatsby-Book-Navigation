f62015279a29c710422d74d462303537ec05df06
Tristan Crain <tcrain@metbiblechurch.local>
2022-11-09 11:32:14 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Portfolio-Gatsby`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
