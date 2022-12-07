0f1e162e33251f8e6083c6272b35262c73f33ec5
Tristan Crain <tcrain@metbiblechurch.local>
2022-12-07 17:10:59 -0500

    Fixed Seasonal Backgrounds Typo

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import SocialNav from "../SocialNavigation";
 import MapNav from "../ArrowNavigation";
 import Map from "../Map";
-import SeasonalBackgrounds from "../seasonalBackgrounds";
+import SeasonalBackgrounds from "../SeasonalBackgrounds";
 
 export default function Layout() {
   return (
